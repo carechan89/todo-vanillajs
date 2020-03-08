52f04f486c7ea6299ba16b4bad024b19e3b86769
Caressa Chan <carechan@umich.edu>
2020-03-07 22:37:49 -0600

    added some css styling, still need to style the checklist items

--- todo.js
+++ todo.js
@@ -8,56 +8,56 @@ let toDoItems = [];
 
 // The actual script part
 nameButton.onclick = function() {
-  let name = prompt("Enter your name");
-  if (name === "" || name === null) {
-    alert("User cancelled the operation.")
-  } else {
-    // TODO: does this local storage work like how i think it does?
-    localStorage.setItem("name", name);
-    title.innerHTML = localStorage.name + "'s to-do list";
-  }
+    let name = prompt("Enter your name");
+    if (name === "" || name === null) {
+        alert("User cancelled the operation.")
+    } else {
+        // TODO: does this local storage work like how i think it does?
+        localStorage.setItem("name", name);
+        title.innerHTML = localStorage.name + "'s to-do list";
+    }
 }
 
 clearAllButton.onclick = function() {
-  toDoItems.splice(0, toDoItems.length);
-  printItems();
-  input.value = "";
+    toDoItems.splice(0, toDoItems.length);
+    printItems();
+    input.value = "";
 }
 
 input.addEventListener("keydown", function(x) {
-  if (x.keyCode === 13) {
-    let myItem = new toDoItem(input.value, "active");
-    toDoItems.push(myItem);
-    printItems();
-    input.value = "";
-  }
+    if (x.keyCode === 13) {
+        let myItem = new toDoItem(input.value, "active");
+        toDoItems.push(myItem);
+        printItems();
+        input.value = "";
+    }
 });
 
 
 // Functions
 function toDoItem(text, status) {
-  this.text = text;
-  this.status = status;
+    this.text = text;
+    this.status = status;
 }
 
 function printItems() {
-  document.getElementById("Result").innerHTML = "";
-  for (let i = 0; i < toDoItems.length; i++) {
-    let checkBox = document.createElement('input');
-    checkBox.type = "checkbox";
-    let checkBoxText = document.createElement('label');
-    checkBoxText.appendChild(document.createTextNode(toDoItems[i].text));
-    let deleteButton = document.createElement('button');
-    deleteButton.id = "delete-button";
-    deleteButton.classList.add("boxclose")
-    deleteButton.onclick = function() {
-      toDoItems.splice(i, 1);
-      printItems();
-    };
-    let newLine = document.createElement('p');
-    myDiv.appendChild(checkBox);
-    myDiv.appendChild(checkBoxText);
-    myDiv.appendChild(deleteButton);
-    myDiv.appendChild(newLine);
-  }
+    document.getElementById("Result").innerHTML = "";
+    for (let i = 0; i < toDoItems.length; i++) {
+        let checkBox = document.createElement('input');
+        checkBox.type = "checkbox";
+        let checkBoxText = document.createElement('label');
+        checkBoxText.appendChild(document.createTextNode(toDoItems[i].text));
+        let deleteButton = document.createElement('button');
+        deleteButton.id = "delete-button";
+        deleteButton.classList.add("boxclose")
+        deleteButton.onclick = function() {
+            toDoItems.splice(i, 1);
+            printItems();
+        };
+        let newLine = document.createElement('p');
+        myDiv.appendChild(checkBox);
+        myDiv.appendChild(checkBoxText);
+        myDiv.appendChild(deleteButton);
+        myDiv.appendChild(newLine);
+    }
 }
